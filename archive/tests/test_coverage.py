05b2d515d2c32ecbb4592fd75ab7b230bcd5277a
Janvi Jain <janvi701jogani@gmail.com>
2026-08-19 20:44:15 +0530

    Clean up unused development files

--- archive/tests/test_coverage.py
+++
@@ -1,9 +0,0 @@
-from modules.topics.coverage import calculate_coverage
-
-sample = [
-    ["SIP", "Taxation", "Risk"],
-    ["SIP", "Taxation"],
-    ["SIP", "Expense Ratio"]
-]
-
-print(calculate_coverage(sample))
